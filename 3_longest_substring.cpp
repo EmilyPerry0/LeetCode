1bbf5c91aaebcc08310514bfcc51c90b1f856643
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-08-24 14:36:08 +0200

    working but slow #3

--- 3_longest_substring.cpp
+++ 3_longest_substring.cpp
@@ -4,19 +4,24 @@ public:
     int lengthOfLongestSubstring(string s) {
       int longestSubstringSoFar = 0;
       map<char, std::string::size_type> charsInCurrSubstring;
-      for(std::string::size_type i = 0; i < s.size(); ++i){
-        for(std::string::size_type j = i; j < s.size(); ++j){
-            if(!charsInCurrSubstring.contains(s[j])){
-                charsInCurrSubstring[s[j]] = j;
-                if(j-i+1 > longestSubstringSoFar){
-                    longestSubstringSoFar = j-i+1;
-                }
-            }else{
-                i = charsInCurrSubstring[s[j]];
-                break;
+      std::string::size_type left_idx = 0;
+      
+      for(std::string::size_type right_idx = 0; right_idx < s.size(); ++right_idx)
+      {
+        if(charsInCurrSubstring.contains(s[right_idx]))
+        {
+            while(left_idx <= charsInCurrSubstring[s[right_idx]])
+            {
+                charsInCurrSubstring.erase(s[left_idx]);
+                ++left_idx;
             }
         }
-        charsInCurrSubstring.clear();
+        charsInCurrSubstring[s[right_idx]] = right_idx;
+
+        if(right_idx - left_idx + 1 > longestSubstringSoFar)
+        {
+            longestSubstringSoFar = right_idx - left_idx + 1;
+        }
       }
       return longestSubstringSoFar;
     }
