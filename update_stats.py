2127fc9bd67ea16d0fe9606faa7c5510a21d0df2
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-08-31 17:45:16 +0200

    removed unnecessary bold markers for the total problems solved line

--- update_stats.py
+++ update_stats.py
@@ -2,7 +2,7 @@ import requests, json
 r = requests.get('https://alfa-leetcode-api.onrender.com/emilyperry0/solved')
 data = r.json()
 with open('README.md', 'w') as f:
-    f.write(f"""## Total Problems Solved: **{data['solvedProblem']}**  
+    f.write(f"""## Total Problems Solved: {data['solvedProblem']}  
     
 Easy: **{data['easySolved']}**  
 Medium: **{data['mediumSolved']}**  
