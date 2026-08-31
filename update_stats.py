a73cb7544b874cfed3e108aff6b8b1aecf79893c
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-08-31 14:26:14 +0200

    fixing f string error

--- update_stats.py
+++ update_stats.py
@@ -2,8 +2,8 @@ import requests, json
 r = requests.get('https://alfa-leetcode-api.onrender.com/emilyperry0/solved')
 data = r.json()
 with open('README.md', 'w') as f:
-    f.write(f"## Total Problems Solved: **{data['solvedProblem']}**
+    f.write(f"""## Total Problems Solved: **{data['solvedProblem']}**  
     
-    Easy: **{data['easySolved']}**
-    Medium: **{data['mediumSolved']}**
-    Hard: **{data['hardSolved']}**")
+    Easy: **{data['easySolved']}**  
+    Medium: **{data['mediumSolved']}**  
+    Hard: **{data['hardSolved']}**""")
