68248e79e989a36a38fffc37a7995d39a06b027d
EmilyPerry0 <arbitrarydragon0@gmail.com>
2026-08-31 14:22:28 +0200

    updated the spacing on the stats page

--- update_stats.py
+++ update_stats.py
@@ -2,4 +2,8 @@ import requests, json
 r = requests.get('https://alfa-leetcode-api.onrender.com/emilyperry0/solved')
 data = r.json()
 with open('README.md', 'w') as f:
-    f.write(f"Total Problems Solved: {data['solvedProblem']}\n\nEasy: {data['easySolved']}\nMedium: {data['mediumSolved']}\nHard: {data['hardSolved']}")
+    f.write(f"## Total Problems Solved: **{data['solvedProblem']}**
+    
+    Easy: **{data['easySolved']}**
+    Medium: **{data['mediumSolved']}**
+    Hard: **{data['hardSolved']}**")
